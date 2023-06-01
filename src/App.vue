09d3f7b18ffad0f9df70f42b781355316e769e38
Tosking <tosking@duck.com>
2023-06-02 00:18:57 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+        <div>
+            Кол-во задач {{ tasks }}
+        </div>
+        <button @click="addTask">
+            Add Task
+        </button>
+    </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+    
+    data(){
+        return{
+            tasks: 0
+        }
+    },
+    methods: {
+        addTask(){
+            this.tasks += 1;
+        }
+    }
 }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="sass">
+    *
+        padding: 0
+        margin: 0
+        box-sizing: border-box
+    .task
+        padding: 15px
+        border: 2px solid
+        margin-top: 10px
+        border-radius: 10px
 </style>
